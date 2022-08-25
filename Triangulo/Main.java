88e36a620d04c4517509003b75e01b5dfb5e3a65
Alejo Agasi <102525836+Alejo55@users.noreply.github.com>
2022-08-25 11:25:11 -0300

    Agrego try-catch para manejo de tipo de dato incorrecto.

--- Triangulo/Main.java
+++ Triangulo/Main.java
@@ -1,26 +1,34 @@
 package Triangulo;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		int ladoA, ladoB, ladoC;
+		int ladoA,ladoB,ladoC;
 		String tipoTriangulo;
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Ingrese el valor del primer lado: ");
-		ladoA = scanner.nextInt();
-		
-		System.out.println("Ingrese el valor del segundo lado: ");
-		ladoB = scanner.nextInt();
-		
-		System.out.println("Ingrese el valor del tercer lado: ");
-		ladoC = scanner.nextInt();
-		
-		scanner.close();
+		try {
+			System.out.println("Ingrese el valor del primer lado: ");
+			ladoA = scanner.nextInt();
+			
+			System.out.println("Ingrese el valor del segundo lado: ");
+			ladoB = scanner.nextInt();
+			
+			System.out.println("Ingrese el valor del tercer lado: ");
+			ladoC = scanner.nextInt();
+			
+		}catch(InputMismatchException e) {
+			System.out.println("El tipo de dato ingresado es incorrecto. Ingrese valores enteros");
+			return;
+			
+		}finally {
+			scanner.close();
+		}
 		
 		if(ladoA <0 || ladoB <0 || ladoC <0) {
-			System.out.println("Error: El valor ingresado debe ser mayor a cero");
+			System.out.println("Error: Los valores ingresados deben ser mayores a cero");
 			return;
 		} 
 		
