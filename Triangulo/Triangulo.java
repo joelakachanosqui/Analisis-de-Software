b6e2884fff6673ce79e2993a61a87659698e7c85
Alejo Agasi <102525836+Alejo55@users.noreply.github.com>
2022-08-25 11:55:16 -0300

    Excepcion para triangulo

--- Triangulo/Triangulo.java
+++ Triangulo/Triangulo.java
@@ -2,22 +2,28 @@ package Triangulo;
 
 public class Triangulo {
 	private int ladoA, ladoB, ladoC;
-	
-	Triangulo(int a, int b, int c) {
-		this.ladoA = a;
-		this.ladoB = b;
-		this.ladoC = c;
-	}
-	
-	public String tipoTriangulo() {
-		if(ladoA == ladoB && ladoB == ladoC) {
-			return new String("Equil·tero");
-		}
+
+	Triangulo(int a, int b, int c) throws TrianguloException {
 		
-		if(ladoA != ladoB && ladoA != ladoC && ladoB != ladoC) {
+		if (a > 0 && b > 0 && c > 0 && (a + b) > c && (a + c) > b && (b + c) > a) {
+			this.ladoA = a;
+			this.ladoB = b;
+			this.ladoC = c;
+			
+		} else {
+			throw new TrianguloException("No puede formarse un tri√°ngulo con las medidas ingresadas.");
+		}
+	}
+
+	public String tipoTriangulo() {
+		if (ladoA == ladoB && ladoB == ladoC) {
+			return new String("Equil√°tero");
+		}
+
+		if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC) {
 			return new String("Escaleno");
 		}
-		
-		return new String("IsÛceles");
+
+		return new String("Is√≥celes");
 	}
 }
