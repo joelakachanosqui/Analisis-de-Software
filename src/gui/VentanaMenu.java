f7e526a88f3e61d20f30bcf2437507eceaf56478
nicol <nicol@DESKTOP-PVJHDJP>
2022-10-05 21:18:08 -0300

    Corrección del exit de la ventana

--- src/gui/VentanaMenu.java
+++ src/gui/VentanaMenu.java
@@ -34,7 +34,7 @@ public class VentanaMenu extends JFrame {
 	 */
 	public VentanaMenu() {
 		setTitle("Sistema Libreria");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 445, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
